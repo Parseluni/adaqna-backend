459d55a484b3d6e449f4242fbd18004dfecbc09f
Delia <deliagaiaglow@gmail.com>
2021-08-04 21:52:53 -0700

    Basic Flask set up

---
+++ app/__init__.py
@@ -0,0 +1,31 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate     # companion package to SQLAlchemy
+from dotenv import load_dotenv
+from flask_cors import CORS
+import os
+
+# Sets up db and migrate, which are conventional variables that give us access to database operations
+db = SQLAlchemy()
+migrate = Migrate()
+load_dotenv()
+
+def create_app(test_config=None):
+    app = Flask(__name__)
+    
+    #if not in test mode
+    if not test_config:
+        # Configures the app to include two new SQLAlchemy settings
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("SQLALCHEMY_DATABASE_URI")
+    else:
+        # test configuration is on
+        app.config["TESTING"] = True
+        app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_TEST_DATABASE_URI")
+    
+    # Connects db and migrate to our Flask app
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    return app
